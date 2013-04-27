fabd805ae108495713907a9ec50d77f4f3f8a30d
stefanthoss <stef@nthoss.de>
2013-04-27 15:32:19 -0700

    Added Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spacefreeze::Application.config.secret_token = '648d37f1c3cca1ecea7c846c962545749646743f1bfa3214f46a930579f97d1128acff7b3de0d60b7eeb4df10f7e4dc1630e86b7312bbe87ae38bc2f3426ba0e'
