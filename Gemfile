b6ce11ea1a8a3492a9aca058e5174ffe6295b175
stefanthoss <stef@nthoss.de>
2013-04-27 16:26:17 -0700

    Added home page, changed routes

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.11'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development do
-  gem 'sqlite3'
+  gem 'sqlite3', '1.3.7'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Spacefreeze::Application.routes.draw do
+  get "home/index"
+
+
   resources :searches
 
 
@@ -58,6 +61,7 @@ Spacefreeze::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
